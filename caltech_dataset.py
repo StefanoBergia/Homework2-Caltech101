63caadc82d46598b6e0e53662c2cbf76c80e4bbf
Stefano Bergia <s276124@studenti.polito.it>
2020-11-22 18:44:45 +0100

    data splitting

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -45,6 +45,8 @@ class Caltech(VisionDataset):
 
 
     def __getitem__(self, index):
+
+        print(self.values[index]+'-'+index)
         image = pil_loader(self.root+'/'+self.values[index])
         label = self.dictionary[self.values[index].split('/')[0]]
 
